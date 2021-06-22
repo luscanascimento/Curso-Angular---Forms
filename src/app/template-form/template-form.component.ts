f8477b3b65895d4295da5dbd5110dba98b11a5e8
Lucas Nascimento <lucasnascimentocontato@gmail.com>
2021-06-22 17:26:41 -0300

    Form - (Template driven) setValue, patchValue(autocompleteCEP) -Não terminado

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-template-form',
@@ -6,14 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-form.component.scss'],
 })
 export class TemplateFormComponent implements OnInit {
+  [x: string]: any;
   usuario: any = {
     nome: null,
     email: null,
   };
-  ngOnInit(): void {}
+
+  constructor(_http: HttpClient) {}
+  ngOnInit() {}
+
+  verificaValidTouched(campo: any) {
+    return !campo.valid && campo.touched;
+  }
+
+  aplicaCssErro(campo: any) {
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo),
+    };
+  }
   onSubmit(formulario: any) {
     console.log(formulario);
     console.log(this.usuario);
   }
-  constructor() {}
+  consultaCEP(cep: string, form) {
+    cep = cep.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado
+    if (cep != '') {
+      //Expressão regular para validar o CEP
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        this.http
+          .get('//viacep.com.br/ws/${cep}/json')
+          .map((dados: { json: () => any }) => dados.json())
+          .subscribe((dados: any) => console.log(dados));
+      }
+    }
+    populaDadosForm(dados, form){
+
+    }
+  }
 }
--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -1,11 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { CampoControlErroComponent } from './../campo-control-erro/campo-control-erro.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
 import { FormsModule } from '@angular/forms';
 import { FormDebugComponent } from '../form-debug/form-debug.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [TemplateFormComponent, FormDebugComponent],
-  imports: [CommonModule, FormsModule],
+  declarations: [
+    FormDebugComponent,
+    CampoControlErroComponent,
+    TemplateFormComponent,
+  ],
+  imports: [CommonModule, FormsModule, BrowserModule, HttpClientModule],
 })
 export class TemplateFormModule {}
